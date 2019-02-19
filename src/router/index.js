69609828883a1c21195d50d1d14e503120f1d36c
kevin16888 <1161276621@qq.com>
2019-02-19 10:00:48 +0800

    局部修改

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,20 @@
-import Vue from "vue";
-import Router from "vue-router";
+import Vue from 'vue'
+import Router from 'vue-router'
 // import Login from '../components/login.vue'
-import Login from "@/components/login.vue";
+import Login from '@/components/login.vue'
 
-Vue.use(Router);
+Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: "/",
-      name: "home"
+      path: '/',
+      name: 'home'
     },
     {
-      name: "login",
-      path: "/login",
+      name: 'login',
+      path: '/login',
       component: Login
     }
   ]
-});
+})
