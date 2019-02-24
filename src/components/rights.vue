55b00d79b30557bda33c2c6e30aea84f27e44f47
kevin16888 <1161276621@qq.com>
2019-02-24 12:17:37 +0800

    部分修改

---
+++ src/components/rights.vue
@@ -0,0 +1,20 @@
+<template>
+  <el-card class="box">
+    <!-- 面包屑 -->
+    <el-breadcrumb separator="/">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>权限管理</el-breadcrumb-item>
+      <el-breadcrumb-item>权限列表</el-breadcrumb-item>
+    </el-breadcrumb>
+  </el-card>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.box {
+  height: 100%;
+}
+</style>
