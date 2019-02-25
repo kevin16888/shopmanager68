d882f982656bc684e8af52c9e0dc7e3fe8c91cd2
kevin16888 <1161276621@qq.com>
2019-02-25 21:47:53 +0800

    展示权限列表

--- src/components/roles.vue
+++ src/components/roles.vue
@@ -9,17 +9,26 @@
           <!-- 行列布局 -->
           <el-row class="level1" v-for="item1 in scope.row.children" :key="item1.id">
             <el-col :span="4">
-              <el-tag closable type="success">{{item1.authName}}</el-tag>
+              <el-tag
+                @close="deleRights(scope.row,item1)"
+                closable
+                type="success"
+              >{{item1.authName}}</el-tag>
               <i class="el-icon-arrow-right"></i>
             </el-col>
             <el-col :span="20">
               <el-row class="level2" v-for="item2 in item1.children" :key="item2.id">
                 <el-col :span="4">
-                  <el-tag closable type="warning">{{item2.authName}}</el-tag>
+                  <el-tag
+                    @close="deleRights(scope.row,item2)"
+                    closable
+                    type="warning"
+                  >{{item2.authName}}</el-tag>
                   <i class="el-icon-arrow-right"></i>
                 </el-col>
                 <el-col :span="20">
                   <el-tag
+                    @close="deleRights(scope.row,item3)"
                     closable
                     type="info"
                     v-for="item3 in item2.children"
@@ -30,9 +39,9 @@
             </el-col>
           </el-row>
           <el-row v-if="scope.row.children.length===0">
-              <el-col>
-                  <span>未分配权限</span>
-              </el-col>
+            <el-col>
+              <span>未分配权限</span>
+            </el-col>
           </el-row>
         </template>
       </el-table-column>
@@ -54,6 +63,27 @@
         </template>
       </el-table-column>
     </el-table>
+    <!-- 对话框 -->
+    <el-dialog title="分配权限" :visible.sync="dialogFormVisible">
+      <!-- props配置选项{label/children}
+      default-checked-keys[]默认选中
+      default-expanded-keys[]默认展开
+      node-key 每个节点唯一标识
+      data数据源-->
+      <el-tree
+        :data="treelist"
+        show-checkbox
+        node-key="id"
+        default-expand-all
+
+        :default-checked-keys="arrCheck"
+        :props="defaultProps"
+      ></el-tree>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
+      </div>
+    </el-dialog>
   </el-card>
 </template>
 
@@ -61,18 +91,86 @@
 export default {
   data() {
     return {
-      roles: []
+      roles: [],
+      dialogFormVisible: false,
+      //树形结构的数据
+      treelist: [],
+      // arrExpand: [],
+      arrCheck: [],
+      defaultProps: {
+        label: "authName",
+        children: "children"
+      }
     };
   },
   created() {
     this.getRoles();
   },
   methods: {
-    //
-    showDiaSetRights() {},
+    //---------------取消权限--------------
+    async deleRights(role, rights) {
+      const res = await this.$http.delete(
+        `roles/${role.id}/rights/${rights.id}`
+      );
+      console.log(res);
+      const {
+        data,
+        meta: { msg, status }
+      } = res.data;
+      if (status === 200) {
+        //1.提示
+        this.$message.success(msg);
+        //2.更新，会返回当前角色的剩余权限
+        // this.getRoles();
+        //只更新当前角色权限
+        role.children = data;
+      }
+    },
+
+    //----------分配权限 ---打开对话框--------------
+    async showDiaSetRights(role) {
+      const res = await this.$http.get(`rights/tree`);
+      // console.log(res);
+      const {
+        data,
+        meta: { msg, status }
+      } = res.data;
+      if (status === 200) {
+        this.treelist = data;
+        //嵌套循环遍历  ---默认角色权限全部展开
+        // const temp = [];
+        // this.treelist.forEach(item1 => {
+        //   temp.push(item1.id);
+        //   item1.children.forEach(item2 => {
+        //     temp.push(item2.id);
+        //     item2.children.forEach(item3 => {
+        //       temp.push(item3.id);
+        //     });
+        //   });
+        // });
+        // // console.log(temp);
+        // this.arrExpand = temp;
+        //获取当前角色有的权限的权限id
+        // console.log(role);
+        const temp2 = [];
+        role.children.forEach(item1 => {
+          // temp2.push(item1.id);最外层id》temp》arrCheck
+          item1.children.forEach(item2 => {
+            // temp2.push(item2.id);
+            item2.children.forEach(item3 => {
+              temp2.push(item3.id);
+            });
+          });
+        });
+        // console.log(temp2);
+        this.arrCheck = temp2;
+        
+      }
+      this.dialogFormVisible = true;
+    },
     async getRoles() {
       const res = await this.$http.get(`roles`);
-      console.log(res);
+      // console.log(res);
       const {
         data,
         meta: { msg, status }
