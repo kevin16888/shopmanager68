99f0f3e6268230307c90bc7e9680e1a2df6dfccf
kevin16888 <1161276621@qq.com>
2019-02-25 12:22:32 +0800

    角色列表-三级权限功能完成

--- src/components/roles.vue
+++ src/components/roles.vue
@@ -4,25 +4,45 @@
     <cus-bread level1="权限管理" level2="角色列表"></cus-bread>
     <el-button class="btn" type="primary">添加角色</el-button>
     <el-table :data="roles" style="width: 100%" height="350px">
-      <el-table-column type="index" label="#" width="180"></el-table-column>
+      <el-table-column type="expand" width="60">
+        <template slot-scope="scope">
+          <!-- 行列布局 -->
+          <el-row class="level1" v-for="item1 in scope.row.children" :key="item1.id">
+            <el-col :span="4">
+              <el-tag closable type="success">{{item1.authName}}</el-tag>
+              <i class="el-icon-arrow-right"></i>
+            </el-col>
+            <el-col :span="20">
+              <el-row class="level2" v-for="item2 in item1.children" :key="item2.id">
+                <el-col :span="4">
+                  <el-tag closable type="warning">{{item2.authName}}</el-tag>
+                  <i class="el-icon-arrow-right"></i>
+                </el-col>
+                <el-col :span="20">
+                  <el-tag
+                    closable
+                    type="info"
+                    v-for="item3 in item2.children"
+                    :key="item3.id"
+                  >{{item3.authName}}</el-tag>
+                </el-col>
+              </el-row>
+            </el-col>
+          </el-row>
+          <el-row v-if="scope.row.children.length===0">
+              <el-col>
+                  <span>未分配权限</span>
+              </el-col>
+          </el-row>
+        </template>
+      </el-table-column>
+      <el-table-column type="index" label="#" width="120"></el-table-column>
       <el-table-column prop="roleName" label="角色名称" width="200"></el-table-column>
       <el-table-column prop="roleDesc" label="角色描述" width="300"></el-table-column>
       <el-table-column label="操作" width="200">
         <template slot-scope="scope">
-          <el-button
-            type="primary"
-            icon="el-icon-edit"
-            circle
-            size="mini"
-            plain
-          ></el-button>
-          <el-button
-            type="danger"
-            icon="el-icon-delete"
-            circle
-            size="mini"
-            plain
-          ></el-button>
+          <el-button type="primary" icon="el-icon-edit" circle size="mini" plain></el-button>
+          <el-button type="danger" icon="el-icon-delete" circle size="mini" plain></el-button>
           <el-button
             @click="showDiaSetRights(scope.row)"
             type="success"
@@ -49,9 +69,7 @@ export default {
   },
   methods: {
     //
-    showDiaSetRights(){
-        
-    },
+    showDiaSetRights() {},
     async getRoles() {
       const res = await this.$http.get(`roles`);
       console.log(res);
@@ -73,5 +91,9 @@ export default {
 }
 .btn {
   margin-top: 20px;
+}
+.level,
+.level2 {
+  margin-bottom: 10px;
 }
 </style>
