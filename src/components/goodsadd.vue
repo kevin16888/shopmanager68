6a3bc626727388779e518495b45c522239fc2cb2
kevin16888 <1161276621@qq.com>
2019-03-03 12:24:55 +0800

    部分修改

--- src/components/goodsadd.vue
+++ src/components/goodsadd.vue
@@ -13,6 +13,7 @@
 
     <el-form class="form" label-position="top" label-width="80px" :model="form">
       <el-tabs @tab-click="changeTab" v-model="active" tab-position="left">
+        
         <el-tab-pane name="1" label="基本信息">
           <el-form-item label="商品名称">
             <el-input v-model="form.goods_name"></el-input>
@@ -27,12 +28,12 @@
             <el-input v-model="form.goods_number"></el-input>
           </el-form-item>
           <el-form-item label="商品分类">
-            {{selectedOptions}}
+            <!-- {{selectedOptions}} -->
             <el-cascader
               clearable
               expand-trigger="hover"
               :options="options"
-              :props="defaultProp"
+              :props="defaultProps"
               v-model="selectedOptions"
               @change="handleChange"
             ></el-cascader>
@@ -54,11 +55,6 @@
 
         <el-tab-pane name="4" label="商品图片">
           <el-form-item>
-<!-- 
-            注意:
-            1.action全路径，axios设置了baseURL》axios的请求不用写baseurl
-            2.headers
- -->
             <el-upload
               action="http://localhost:8888/api/private/v1/upload"
               :headers="headers"
@@ -75,7 +71,7 @@
           <el-form-item>
             <el-button @click="addGoods()">添加商品</el-button>
             <quill-editor v-model="form.goods_introduce"></quill-editor>
-          </el-form-item>  
+          </el-form-item>
         </el-tab-pane>
 
       </el-tabs>
@@ -84,11 +80,11 @@
 </template>
 
 <script>
-import 'quill/dist/quill.core.css'
-import 'quill/dist/quill.snow.css'
-import 'quill/dist/quill.bubble.css'
- 
-import { quillEditor } from 'vue-quill-editor'
+import "quill/dist/quill.core.css";
+import "quill/dist/quill.snow.css";
+import "quill/dist/quill.bubble.css";
+
+import { quillEditor } from "vue-quill-editor";
 
 export default {
   components: {
@@ -96,43 +92,46 @@ export default {
   },
   data() {
     return {
-      active: "2",
+      active: "1",
       //添加商品的表单数据
       // goods_name	商品名称	不能为空
       // goods_price	价格	不能为空
       // goods_weight	重量	不能为空
       // goods_number	数量	不能为空
       // goods_introduce	介绍	可以为空
-      
+
       // goods_cat	以为','分割的分类列表	不能为空
       // pics	上传的图片临时路径（对象）	可以为空
       // attrs	商品的参数（数组）	可以为空
       form: {
         goods_name: "",
-        goods_cat: "",
         goods_price: "",
         goods_number: "",
         goods_weight: "",
         goods_introduce: "",
+        //分类id
+        goods_cat: "",
         pics: [],
         attrs: ""
       },
-      // 级联使用的数据
+      //层级下拉框的数据源
       options: [],
+      //绑定到层级下拉框上的数据
       selectedOptions: [1, 3, 6],
-      defaultProp: {
+      //层级下拉框的数据源配置
+      defaultProps: {
         value: "cat_id",
         label: "cat_name",
         children: "children"
       },
       //复选框组
-      // checkList: [],
+      checkList: [],
       //动态参数数组
       arrDy: [],
       //静态参数数组
       arrStatic: [],
-      headers:{
-        Authorization:localStorage.getItem("token")
+      headers: {
+        Authorization: localStorage.getItem("token")
       }
     };
   },
@@ -141,21 +140,56 @@ export default {
   },
   methods: {
     //添加商品
-    async addGoods(){
-      const res = await this.$http.post(`goods`,this.form);
-      console.log(res);
-      
+    async addGoods() {
+      //1.处理goods_cat
+      this.form.goods_cat = this.selectedOptions.join(",")
+      // const res = await this.$http.post(`goods`,this.form);
+      //2.this.form.pics->在图片上传方法中splice和push
+      //3.attrs[{attr_id:?,attr_value:?}]
+      //动态参数数组，遍历，返回数组，return "abc", this.arrDy
+      const arr1 = this.arrDy.map((item) => {
+        return { attr_id: item.attr_id, attr_value: item.attr_vals };
+      });
+      //静态数组
+      const arr2 = this.arrStatic.map((item) => {
+        return { attr_id: item.attr_id, attr_value: item.attr_vals };
+      });
+      this.form.attrs = [...arr1, ...arr2];
+      const res = await this.$http.post('goods', this.form);
+      const {
+        meta: { msg, status },
+        data
+      } = res.data;
+      if (status === 201) {
+        //提示
+        this.$message.success(msg);
+        //回到列表
+        this.$router.push({
+          name: "goods"
+        });
+      } else {
+        console.log(msg);
+      }
     },
     //图片上传方法
     handleRemove(file, fileList) {
-        console.log("file---");
-        //临时路径
-        file.response.data.tmp_path;
-      },
-      handleSuccess(response, file, fileList){
-        console.log("success----");
-        // response.data.tmp_path; --临时路径
-      },
+      console.log("file---");
+      //临时路径
+      // file.response.data.tmp_path;
+      //遍历, return a>b , 返回索引
+      //findIndex方法(ES6数组新增API)
+      const index = this.form.pics.findIndex((item) => {
+        // console.log(item);
+        return item.pic === file.response.data.tmp_path;
+      });
+      this.form.pics.splice(index, 1);
+    },
+    handleSuccess(response, file, fileList) {
+      // response.data.tmp_path; --临时路径
+      this.form.pics.push({
+        pic: response.data.tmp_path
+      });
+    },
     //点击任何tab都会
     async changeTab() {
       //如果点了第二个tab
@@ -164,6 +198,12 @@ export default {
         if (this.selectedOptions.length !== 3) {
           //提示
           this.$message.error("请选择三级分类！");
+          //解决bug
+          if(this.active === "2"){
+            this.arrDy = []
+          } else {
+            this.arrStatic = []
+          }
           return;
         }
         //获取静态数组
@@ -171,7 +211,6 @@ export default {
           const res = await this.$http.get(
             `categories/${this.selectedOptions[2]}/attributes?sel=only`
           );
-          // console.log(res);
           const {
             meta: { msg, status },
             data
@@ -187,22 +226,19 @@ export default {
           const res = await this.$http.get(
             `categories/${this.selectedOptions[2]}/attributes?sel=many`
           );
-          // console.log(res);
           const {
             meta: { msg, status },
             data
           } = res.data;
           if (status === 200) {
             this.arrDy = data;
-            // console.log(this.arrDy);
-            this.arrDy.forEach(item => {
+            this.arrDy.forEach((item) => {
               //判断后台如果返回空字符串
               //   //如果不是，则split方法
               item.attr_vals =
                 item.attr_vals.trim().length === 0
                   ? []
                   : item.attr_vals.trim().split(",");
-              // console.log(item.attr_vals);
             });
           }
         }
@@ -211,7 +247,6 @@ export default {
     //获取三级商品分类数据
     async getGoodsCate() {
       const res = await this.$http.get(`categories?type=3`);
-      // console.log(res);
       const {
         meta: { msg, status },
         data
@@ -236,7 +271,8 @@ export default {
   height: 350px;
   overflow: auto;
 }
-.ql-editor,.ql-blank {
+.ql-editor,
+.ql-blank {
   min-height: 200px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -12,12 +12,9 @@ import HttpServer from '@/http.js'
 
 // 把不是vue插件的axios变成vue插件
 Vue.use(HttpServer)
-
 Vue.use(ElementUI)
-
-
-
 Vue.config.productionTip = false
+Vue.component(CusBread.name,CusBread);
 
 //全局过滤器---处理日期格式
 Vue.filter('fmtdate',(v)=>{
@@ -25,7 +22,6 @@ Vue.filter('fmtdate',(v)=>{
 })
 
 //全局自定义面包屑组件
-Vue.component(CusBread.name,CusBread);
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
